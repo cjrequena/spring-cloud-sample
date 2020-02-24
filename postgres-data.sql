344fbc4a1e13fbaf1fa43abd84b6a771aa1798e6
cjrequena <requenacj@gmail.com>
2020-02-24 10:45:11 +0100

    misc

--- postgres-data.sql
+++ postgres-data.sql
@@ -1,3 +1,6 @@
+
+\c foo
+
 INSERT INTO FOO (NAME, DESCRIPTION) VALUES ('baking soda','white powder used for cleaning and removing odours');
 INSERT INTO FOO (NAME, DESCRIPTION) VALUES ('bleach','liquid added in small amounts to water; has strong chemicals that remove stains on white clothing; also used to clean bathrooms');
 INSERT INTO FOO (NAME, DESCRIPTION) VALUES ('broom','brush with a long handle on it for sweeping floors');
