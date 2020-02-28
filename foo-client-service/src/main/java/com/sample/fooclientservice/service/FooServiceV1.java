8b502cbf2bdcde3046339779c59e50a37357fdbe
Carlos Requena <requenacj@gmail.com>
2020-02-28 10:49:16 +0100

    misc

--- foo-client-service/src/main/java/com/sample/fooclientservice/service/FooServiceV1.java
+++ foo-client-service/src/main/java/com/sample/fooclientservice/service/FooServiceV1.java
@@ -3,30 +3,18 @@ package com.sample.fooclientservice.service;
 import com.sample.fooclientservice.dto.FooDTOV1;
 import com.sample.fooclientservice.exception.EErrorCode;
 import com.sample.fooclientservice.exception.ServiceException;
+import com.sample.fooclientservice.service.feign.FooServerServiceV1Feign;
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.json.JsonMergePatch;
 import javax.json.JsonPatch;
 import java.util.List;
-
-import static com.sample.fooclientservice.common.Constant.VND_FOO_SERVICE_V1;
 
 /**
  * <p>
@@ -42,19 +30,18 @@ import static com.sample.fooclientservice.common.Constant.VND_FOO_SERVICE_V1;
 public class FooServiceV1 {
 
   @Autowired
-  FooServerServiceV1Feign fooServerServiceV1Feign;
-  private static final String FOO_SERVER_SERVICE = "foo-server-service";
+  private FooServerServiceV1Feign fooServerServiceV1Feign;
+  private static final String FOO_SERVICE = "foo-service";
 
   /**
    *
-   * @param dto
-   * @return
+   * @param dto   * @return
    * @throws ServiceException
    */
-  //@TimeLimiter(name = FOO_SERVER_SERVICE)
-  @CircuitBreaker(name = FOO_SERVER_SERVICE, fallbackMethod = "createFallbackMethod")
-  @Bulkhead(name = FOO_SERVER_SERVICE)
-  @Retry(name = FOO_SERVER_SERVICE)
+  //@TimeLimiter(name = FOO_SERVICE)
+  @CircuitBreaker(name = FOO_SERVICE, fallbackMethod = "createFallbackMethod")
+  @Bulkhead(name = FOO_SERVICE)
+  @Retry(name = FOO_SERVICE)
   public ResponseEntity<Void> create(FooDTOV1 dto) throws ServiceException {
     //--
     try {
@@ -89,10 +76,10 @@ public class FooServiceV1 {
    * @return
    * @throws ServiceException
    */
-  //@TimeLimiter(name = FOO_SERVER_SERVICE)
-  @CircuitBreaker(name = FOO_SERVER_SERVICE, fallbackMethod = "retrieveByIdFallbackMethod")
-  @Bulkhead(name = FOO_SERVER_SERVICE)
-  @Retry(name = FOO_SERVER_SERVICE)
+  //@TimeLimiter(name = FOO_SERVICE)
+  @CircuitBreaker(name = FOO_SERVICE, fallbackMethod = "retrieveByIdFallbackMethod")
+  @Bulkhead(name = FOO_SERVICE)
+  @Retry(name = FOO_SERVICE)
   public ResponseEntity<FooDTOV1> retrieveById(Long id) throws ServiceException {
     //--
     try {
@@ -226,123 +213,4 @@ public class FooServiceV1 {
     //--
   }
 
-
-  /**
-   *
-   */
-  //@FeignClient(url = "http://localhost:9080/foo-server-service")
-  @FeignClient(name = "foo-server-service", path = "/foo-server-service")
-  @RequestMapping(headers = {"Accept-Version=" + VND_FOO_SERVICE_V1})
-  public interface FooServerServiceV1Feign {
-
-    /**
-     *
-     * @param dto
-     * @return
-     * @throws ServiceException
-     */
-    @PostMapping(value = "/fooes",
-      produces = {
-        MediaType.APPLICATION_JSON_VALUE
-      }
-    )
-    ResponseEntity<Void> create(@RequestBody FooDTOV1 dto) throws ServiceException;
-
-    /**
-     *
-     * @param id
-     * @return
-     * @throws ServiceException
-     */
-    @GetMapping(
-      path = "/fooes/{id}",
-      produces = {
-        MediaType.APPLICATION_JSON_VALUE
-      }
-    )
-    ResponseEntity<FooDTOV1> retrieveById(@PathVariable(value = "id") Long id) throws ServiceException;
-
-    /**
-     *
-     * @param fields
-     * @param filters
-     * @param sort
-     * @param offset
-     * @param limit
-     * @return
-     * @throws ServiceException
-     */
-    @GetMapping(
-      path = "/fooes",
-      produces = {
-        MediaType.APPLICATION_JSON_VALUE
-      }
-    )
-    ResponseEntity<List<FooDTOV1>> retrieve(
-      @RequestParam(value = "fields") String fields,
-      @RequestParam(value = "filters") String filters,
-      @RequestParam(value = "sort") String sort,
-      @RequestParam(value = "offset") Integer offset,
-      @RequestParam(value = "limit") Integer limit) throws ServiceException;
-
-    /**
-     *
-     * @param id
-     * @param dto
-     * @return
-     * @throws ServiceException
-     */
-    @PutMapping(
-      path = "/fooes/{id}",
-      produces = {
-        MediaType.APPLICATION_JSON_VALUE
-      }
-    )
-    ResponseEntity<Void> update(
-      @PathVariable(value = "id") Long id,
-      @RequestBody FooDTOV1 dto
-    ) throws ServiceException;
-
-    /**
-     *
-     * @param id
-     * @param patchDocument
-     * @return
-     * @throws ServiceException
-     */
-    @PatchMapping(
-      path = "/fooes/{id}",
-      produces = {MediaType.APPLICATION_JSON_VALUE},
-      consumes = {"application/json-patch+json"}
-    )
-    ResponseEntity<Void> patch(@PathVariable(value = "id") Long id, @RequestBody JsonPatch patchDocument) throws ServiceException;
-
-    /**
-     *
-     * @param id
-     * @param mergePatchDocument
-     * @return
-     * @throws ServiceException
-     */
-    @PatchMapping(
-      path = "/fooes/{id}",
-      produces = {MediaType.APPLICATION_JSON_VALUE},
-      consumes = {"application/merge-patch+json"}
-    )
-    ResponseEntity<Void> patch(@PathVariable(value = "id") Long id, @RequestBody JsonMergePatch mergePatchDocument) throws ServiceException;
-
-    /**
-     *
-     * @param id
-     * @return
-     * @throws ServiceException
-     */
-    @DeleteMapping(
-      path = "/fooes/{id}",
-      produces = {
-        MediaType.APPLICATION_JSON_VALUE
-      }
-    )
-    ResponseEntity<Void> delete(@PathVariable(value = "id") Long id) throws ServiceException;
-  }
 }
